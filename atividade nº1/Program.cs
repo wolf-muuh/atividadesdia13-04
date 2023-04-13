d3d398818753a39ea6468343315c0fc0237f3222
Murilo Alves Zapiello <54750759805@info.local>
2023-04-13 17:27:03 -0300

    aula dia 13/04

---
+++ atividade nº1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga seela poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).
+
+DateOnly actualDate = DateOnly.FromDateTime(DateTime.Now);
+
+Console.WriteLine($"Digite seu ano de nascimento: ");
+int anoNasc = int.Parse(Console.ReadLine());
+int anoAtual = actualDate.Year;
+
+
+comp(anoNasc, anoAtual);
+
+static void comp(int anoNasc, int anoAtual)
+{
+    if (anoAtual - anoNasc >= 16)
+    {
+        Console.WriteLine($"Pode votar ;)");
+
+    }
+    else if (anoAtual - anoNasc < 16)
+    {
+        Console.WriteLine($"Não pode votar não :(");
+
+    }
+    else
+    {
+        Console.WriteLine($"Você acabou de nascer o que você ta fazendo aqui???");
+    }
+}
+
---
+++ atividade nº2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+// Álcool: até 20 litros, desconto de 3% por litro Álcool acima de 20 litros, desconto de 5% por litro
+// Gasolina: até 20 litros, desconto de 4% por litro Gasolina acima de 20 litros, desconto de 6% por litro
+
+// Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 5,30 e o preço do litro do álcool é R$ 4,90. Dica: utilize switch case e funções/métodos para otimizar o algorítimo.
+
+Console.WriteLine(@$"
+-------------------------------------------------------
+|                                                     |
+|         Preço da gasolina = 5,30 o litro            |
+| Digite G para escolher encher o tanque com gasolina |
+|         Preço do alcool = 4,90 o litro              |
+| Digite A para escolher encher o tanque com gasolina |
+|                                                     |
+-------------------------------------------------------
+
+");
+
+char opcao = char.Parse(Console.ReadLine());
+
+
+
+
+while (opcao != 'G' && opcao != 'g' && opcao != 'A' && opcao != 'a')
+{
+    
+}
+if (opcao == 'G' || opcao == 'g'){
+    Console.WriteLine($"Digite quantos litros de gasolina deseja: ");
+    float litrosGas = float.Parse(Console.ReadLine());
+}
+else if (opcao == 'A' && opcao == 'a') {
+    Console.WriteLine($"Digite quantos litros de álcool: ");
+    float litrosAlcool = float.Parse(Console.ReadLine());
+}
